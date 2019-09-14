7fe3092953f28b54e75ad5eb0bf86574d294f547
Rory Martin <a1740203@student.adelaide.edu.au>
2019-09-14 21:44:46 +0930

    adding OS.cpp

---
+++ OperatingSystem.cpp
@@ -0,0 +1,21 @@
+#include <fstream>
+#include <sstream>
+#include <iostream>
+#include <string>
+#include <vector>
+#include "Customer.hpp"
+#include "Queue.hpp"
+
+using namespace std;
+
+void operatingSystem() {
+    
+    cout << "fff";
+    
+    for (int i = 0; i < 1000; i++) {
+        
+    }
+    
+    
+    return 0;
+}
